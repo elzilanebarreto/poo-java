ca1cee27fa24034a9034c8780d7f2df9c0563d68
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-17 13:20:10 -0400

    Classe de execução do carro

--- exercicios/src/MainCarro.java
+++ exercicios/src/MainCarro.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class MainCarro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Carro carro = new Carro();
+
+        System.out.println("Informe o modelo do carro: ");
+        carro.modelo = sc.nextLine();
+
+        System.out.println("Informe a cor do carro: ");
+        carro.cor = sc.nextLine();
+
+        System.out.println("Informe o ano de lançamento do carro: ");
+        carro.anoDeLancamento = sc.nextInt();
+
+        carro.fichaTecnica();
+        carro.calcularIdadeDoCarro();
+
+
+        sc.close();
+    }
 
 }
