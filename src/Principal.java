81fbf3472519c4fc814afe5509ef82deff50baaa
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-16 22:39:55 -0400

    Adicionando métodos na classe

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Instanciando um objeto
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        System.out.println("Informe o nome do filme: ");
+        meuFilme.nome = sc.nextLine();
 
-        System.out.println("Nome: " + meuFilme.nome);
+        System.out.println("Informe o ano de lançamento do filme: ");
+        meuFilme.anoDeLancamento = sc.nextInt();
+
+//        System.out.println("Informe a nota do filme: ");
+//        double notasDeAvalicacao = sc.nextDouble();
+
+        System.out.println("Informe a duração em minutos do filme: ");
+        meuFilme.duracaoEmMinutos = sc.nextInt();
+
+        meuFilme.avaliarFilme(8.5);
+        meuFilme.avaliarFilme(5.5);
+        meuFilme.avaliarFilme(6.5);
+
+        meuFilme.exibirFichaTecnica();
+        System.out.println("Soma das avaliações: " + meuFilme.somaDasAvaliacoes);
+        System.out.printf("A média das avaliações: %.2f", meuFilme.retornarMedia());
+
+        sc.close();
     }
 }
