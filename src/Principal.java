f2a30bb119d58866e5b7f70c896dc99b69d4c032
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-20 11:49:49 -0400

    Encapsulamento

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,5 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 import java.util.Scanner;
 
 public class Principal {
@@ -8,24 +10,28 @@ public class Principal {
         Filme meuFilme = new Filme();
 
         System.out.println("Informe o nome do filme: ");
-        meuFilme.nome = sc.nextLine();
+        String nome = sc.nextLine();
+        meuFilme.setNome(nome);
 
         System.out.println("Informe o ano de lançamento do filme: ");
-        meuFilme.anoDeLancamento = sc.nextInt();
+        int ano = sc.nextInt();
+        meuFilme.setAnoDeLancamento(ano);
 
 //        System.out.println("Informe a nota do filme: ");
 //        double notasDeAvalicacao = sc.nextDouble();
 
         System.out.println("Informe a duração em minutos do filme: ");
-        meuFilme.duracaoEmMinutos = sc.nextInt();
+        int tempo = sc.nextInt();
+        meuFilme.setDuracaoEmMinutos(tempo);
+
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.avaliarFilme(8.5);
         meuFilme.avaliarFilme(5.5);
         meuFilme.avaliarFilme(6.5);
 
         meuFilme.exibirFichaTecnica();
-        System.out.println("Soma das avaliações: " + meuFilme.somaDasAvaliacoes);
-        System.out.printf("A média das avaliações: %.2f", meuFilme.retornarMedia());
+        System.out.printf("A média das avaliações: %.2f", meuFilme.pegarMedia());
 
         sc.close();
     }
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,68 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+
+    // Atributos encapsulados
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //Métodos acessores (get e set)
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return this.anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean getIncluidoNoPlano() {
+        return this.incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return this.duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return this.totalDeAvaliacoes;
+    }
+
+    //Métodos
+    public void exibirFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Incluído no plano: " + incluidoNoPlano);
+        System.out.println("Total de avaliações: " + totalDeAvaliacoes);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+    }
+
+    public void avaliarFilme(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegarMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+}
